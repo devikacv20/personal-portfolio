a4acab9a0aeb9fb75af6d285d2f5c5c31fce1186
devikacv20 <devikacviju@gmail.com>
2025-07-23 08:39:46 +0530

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 //https://astro.build/config
 export default defineConfig({
   site: 'https://rishibharadwajsai.vercel.app',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: 'static'
 });
